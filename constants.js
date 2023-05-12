9698147e8b8a67351c46507b27667bf56b1ff0d5
Bertuğ Kara <bertugkara@outlook.com>
2023-05-12 21:54:57 +0300

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT || 3002;
 const LOCALHOST = process.env.LOCALHOST;
-const celestia_wallet = "celestia1p20jz5tc50agrlr8ud5f87r83k23t2l8axvt34";
+const celestia_wallet = "YOUR_CELESTIA_LIGHT_NODE_WALLET_ADDRESS_HERE";
 const CELESTIA_PREFIX = "celestia";
 const GAS_LIMIT = 80000;
 const FEE = 2000;
