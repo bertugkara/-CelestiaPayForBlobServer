385ca2d17caf60dac465a80b467732e64ad945fb
bertugkara <bertugkara@outlook.com>
2023-05-13 15:44:22 +0300

    Celestia PFB Server corrections

--- constants.js
+++ constants.js
@@ -1,15 +1,16 @@
 const port = process.env.PORT || 3002;
-const LOCALHOST = process.env.LOCALHOST;
-const celestia_wallet = "celestia1p20jz5tc50agrlr8ud5f87r83k23t2l8axvt34";
+const celestia_wallet = process.env.WALLET_ADDRESS; // go to your terminal and type
+// "export WALLET_ADDRESS=<the imported celestia address on your node which you will spend your PFB fees.>"
 const CELESTIA_PREFIX = "celestia";
 const GAS_LIMIT = 80000;
 const FEE = 2000;
+const PFB_URL = `http://localhost:26659/submit_pfb`
 
 module.exports = {
     port: port,
-    LOCALHOST: LOCALHOST,
     celestia_wallet: celestia_wallet,
     CELESTIA_PREFIX: CELESTIA_PREFIX,
     GAS_LIMIT: GAS_LIMIT,
     FEE: FEE,
-}
+    PFB_URL: PFB_URL
+};
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const keplr = require('@keplr-wallet/cosmos')
 const axios = require("axios");
 const amino = require("@cosmjs/amino");
 const {isEmpty} = require("lodash");
-const {port, CELESTIA_PREFIX, celestia_wallet, FEE, GAS_LIMIT, LOCALHOST} = require("./constants");
+const {port, CELESTIA_PREFIX, celestia_wallet, FEE, GAS_LIMIT, PFB_URL} = require("./constants");
 
 const app = express();
 app.use(express.json());
@@ -13,41 +13,46 @@ app.use(cors());
 // start the server
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server listening on port ${port}`);
+    console.log("Your Node Wallet:", celestia_wallet)
 });
 
-app.post('/sendTx', (request, response) => {
+app.post('/api/sendTx', (request, response) => {
     const {signature, namespaceId, data, signedMessage} = request.body;
     verifySignature(signature, namespaceId, data, signedMessage).then((result) => {
         if (result) {
-            axios.post(`${LOCALHOST}:26659/submit_pfb`, {
-                namespaceId, data, GAS_LIMIT, FEE
-            }).then(async (tx_res) => {
-                let tx_data = await tx_res.data
-                response.status(200).send(tx_data)
-            }).catch((err) => {
-                console.log(err)
-                response.status(500).send("Error in submitting PFB");
-            });
+            console.log("Sending...")
+            axios.post(PFB_URL, JSON.stringify({
+                namespace_id: namespaceId,
+                data: data,
+                gas_limit: GAS_LIMIT,
+                fee: FEE
+            }))
+                .then((tx_res) => {
+                    let tx_data = tx_res.data
+                    console.log(tx_data)
+                    response.status(200).send(tx_data)
+                })
+                .catch((err) => {
+                    console.log(err)
+                    response.status(500).send("Error happened while submitting PFB");
+                });
+        } else {
+            response.status(401).send("You are not owner of this node!");
         }
     }).catch((err) => {
-        response.status(404).send(err);
+        response.status(500).send(err);
     });
 });
 
 const verifySignature = async (signature, namespaceId, data, signedMessage) => {
-    try {
-        const address = amino.pubkeyToAddress(signature.pub_key, CELESTIA_PREFIX);
-        console.log(address)
-        const {pubkey: decodedPubKey, signature: decodedSignature} = amino.decodeSignature(signature);
-        let result = keplr.verifyADR36Amino(CELESTIA_PREFIX, address, signedMessage, decodedPubKey, decodedSignature);
-        console.log(result)
-        if (result && celestia_wallet === address && !isEmpty(namespaceId) && !isEmpty(data)) {
-            console.log("Signature Verified, Ready to send Transaction!")
-            return result;
-        }
-    } catch (error) {
-        console.log(error)
+    const address = amino.pubkeyToAddress(signature.pub_key, CELESTIA_PREFIX);
+    const {pubkey: decodedPubKey, signature: decodedSignature} = amino.decodeSignature(signature);
+    let result = keplr.verifyADR36Amino(CELESTIA_PREFIX, address, signedMessage, decodedPubKey, decodedSignature);
+    if (result && celestia_wallet === address && !isEmpty(namespaceId) && !isEmpty(data)) {
+        console.log("Signature Verified, Ready to send Transaction!")
+        return result;
+    } else {
+        console.log("Signature Verification Failed!")
         return false;
     }
-
 }
